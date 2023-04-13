5d2cb3570cc87dcb75559411020d6bb9e1d6f368
mrprotocoll <lekanvgbg@gmail.com>
2023-04-13 11:40:21 +0100

    Added Student class

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require_relative 'model/person'
 
-person = Person.new(17,parent_permission: false)
+person = Person.new(17, parent_permission: false)
 p(person.can_use_services?)
--- model/student.rb
+++ model/student.rb
@@ -1,1 +1,10 @@
-class Student
+class Student < Person
+  def initialize(classroom)
+    super()
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
