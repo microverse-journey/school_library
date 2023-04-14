a367dcdf0b2506270a34ead493ba81aeea0b6b7e
mrprotocoll <lekanvgbg@gmail.com>
2023-04-14 13:45:07 +0100

    Added app class

--- main.rb
+++ main.rb
@@ -1,9 +1,58 @@
-require_relative 'model/person'
-require_relative 'model/nameable'
+class App
+  attr_reader :books, :people
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p(capitalized_trimmed_person.correct_name)
+  def initialize
+    introduction
+    # user input
+    @option = gets.chomp
+    @books = []
+    @people = []
+    option(@option)
+  end
+
+  def add_book(title, author)
+    print 'Please enter book title: '
+    title = gets.chomp
+    print 'Please enter book author: '
+    author = gets.chomp
+    @books << Book.new(title, author)
+    print "book titled #{title} and written by #{author} added successfully"
+  end
+
+  def introduction
+    puts ''
+    puts '             OOP SCHOOL LIBRARY'
+    puts '----------------------------------------------------'
+    puts ''
+    puts 'Please choose an option by entering a number:'
+    puts ''
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    puts ''
+    print 'Enter number: '
+  end
+
+  def option(option)
+    case option
+    when '1' then puts self.books
+    when '2' then add_book
+    when '3' then add_book
+    when '4' then app.new_book
+    when '5' then app.new_rental
+    when '6' then app.list_rentals
+    else
+      puts 'That is not a valid option'
+    end
+  end
+end
+
+def main
+  App.new
+end
+
+main
