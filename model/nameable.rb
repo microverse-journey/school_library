b72f79d5887701e02df665aeea24108d65fc10b2
mrprotocoll <lekanvgbg@gmail.com>
2023-04-14 10:03:19 +0100

    Added capitalize and trimmer decorators

--- model/nameable.rb
+++ model/nameable.rb
@@ -4,3 +4,25 @@ class Nameable
   end
 
 end
+
+class baseDecorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable.new
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < baseDecorator
+  def correct_name
+    (@nameable.correct_name).capitalize()
+  end
+end
+
+class TrimmerDecorator < baseDecorator
+  def correct_name
+    (@nameable.correct_name).truncate(10)
+  end
+end
